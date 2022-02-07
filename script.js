be8f59d095eab964ae6fca61ede484811f55f7f7
DanielVec <vechioladaniel@gmail.com>
2022-02-06 22:52:38 -0800

    add a reset button and fix container to one size

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-
-function makeSquares() {
-    for (i = 0; i < 16; i++) {
+makeSquares(16);
+function makeSquares(numSquares) {
+    document.querySelector('#container').innerHTML = '';
+    for (i = 0; i < numSquares; i++) {
         let row = document.createElement('div');
         row.className = "row"
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < numSquares; j++) {
             let column = document.createElement('div');
             column.className = "column"
             row.appendChild(column);
+            column.style.width = (960/numSquares) + "px";
+            column.style.height = (960/numSquares) + "px";
         }
         document.getElementById("container").appendChild(row);
-    }
+    } colorSquares();
 }
 
-makeSquares();
 
-/*function colorChanger() {
-    document.getElementsByClassName(".column").className = "newColor";
-}*/
+button = document.querySelector('button')
+button.addEventListener('click', reload)
+
+let squares = Array.from(document.querySelectorAll(".column"));
+
+function reload() {
+    let numOfSquares = parseInt(prompt("How many squares per grid? (max 100)"));
+    //squares.forEach(square => square.style.backgroundColor = 'black');
+    makeSquares(numOfSquares);
+    //colorSquares();
+   
+}
 
 
-const squares = Array.from(document.querySelectorAll(".column"));
-squares.forEach(square => square.addEventListener('mouseover', () => {square.style.backgroundColor = 'black';}));
+function colorSquares() {
+    
+    document.querySelectorAll(".column").forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add("newColor");
+        });
+    })
+}
