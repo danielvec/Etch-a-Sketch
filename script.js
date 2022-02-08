e602b9a2ba5b149937a1a01a1569c436b373ff98
DanielVec <vechioladaniel@gmail.com>
2022-02-07 22:00:49 -0800

    style page, fit squares all into one container by removing border

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 makeSquares(16);
+//create a given number of squares to be drawn on
 function makeSquares(numSquares) {
     document.querySelector('#container').innerHTML = '';
     for (i = 0; i < numSquares; i++) {
@@ -8,31 +9,32 @@ function makeSquares(numSquares) {
             let column = document.createElement('div');
             column.className = "column"
             row.appendChild(column);
-            column.style.width = (960/numSquares) + "px";
+            column.style.minWidth = (960/numSquares) + "px";
             column.style.height = (960/numSquares) + "px";
         }
         document.getElementById("container").appendChild(row);
     } colorSquares();
 }
 
-
 button = document.querySelector('button')
 button.addEventListener('click', reload)
 
 let squares = Array.from(document.querySelectorAll(".column"));
-
+//clear all squares, allow user to choose number of squares
 function reload() {
-    
+    document.querySelectorAll(".column").forEach(square => {
+        square.classList.remove("newColor"); });
     let numOfSquares = parseInt(prompt("How many squares per grid? (max 100)"));
-    //squares.forEach(square => square.style.backgroundColor = 'black');
-    makeSquares(numOfSquares);
-    //colorSquares();
-   
+    if (numOfSquares < 101) {
+     makeSquares(numOfSquares);
+    }
+    else {
+        numOfSquares = parseInt(prompt("Not a valid number. How many squares per grid? (max 100)"));
+        makeSquares(numOfSquares);
+    }
 }
-
-
+//change square color to black once hovered over 
 function colorSquares() {
-    
     document.querySelectorAll(".column").forEach(square => {
         square.addEventListener('mouseover', () => {
             square.classList.add("newColor");
